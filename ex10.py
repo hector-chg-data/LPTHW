b5ecead43ece03fa6133839a0774176c48b8ff17
Héctor <hector@PB.s>
2018-04-29 13:49:51 +0200

    New files and renaming files

---
+++ ex10.py
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a new line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat= """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+####################
+
+import subprocess as sp
+
+while True:
+    for i in ["/","-","\\","|"]:
+        print("%s\r" %(i))
+        sp.call('clear', shell=True)
---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv as agv
+
+script, first, second, third = agv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second varieble is:", second)
+print("Your third variable is:", third)
